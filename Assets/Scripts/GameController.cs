006fa664094a8fd3676f60f1bbcbbbf97e248be1
kkhanipov <32584968+kkhanipov@users.noreply.github.com>
2017-11-29 21:19:59 -0600

    added metalcrate damage

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
 {
     // Crates
     public GameObject crate;
+    public GameObject metal_crate;
     public GameObject boat;
     //public GameObject net;
     public Transform droppingPoint;
@@ -75,7 +76,8 @@ public class GameController : MonoBehaviour
             float z = 0;
             Vector3 pos = new Vector3(x, y, z);
             droppingPoint.position = pos;
-            Instantiate(crate, droppingPoint.position, droppingPoint.rotation);
+            if (Random.Range(0, 10) < 3) Instantiate(metal_crate, droppingPoint.position, droppingPoint.rotation);
+            else Instantiate(crate, droppingPoint.position, droppingPoint.rotation);
             yield return new WaitForSeconds(dropRate);
         }
     }
@@ -96,6 +98,18 @@ public class GameController : MonoBehaviour
     {
              
         boat_health = boat_health - 1;
+        if(boat_health<=0)
+        {
+            SceneManager.LoadScene(3);
+            //end game
+        }
+        playerHealthSlider.value=boat_health;
+       // playerHealthSlider.
+    }
+    public void Metalcrate_Damage()
+    {
+             
+        boat_health = boat_health - 10;
         if(boat_health<=0)
         {
             SceneManager.LoadScene(3);
---
+++ Assets/Scripts/metalcratecontroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class metalcratecontroller : MonoBehaviour
+{
+
+    public float thrust;
+    public Rigidbody2D rb;
+    public int scoreValue;
+    private GameController gameController;
+    public GameObject windZone;
+    public bool isWindZone = false;
+    private Animator amin;
+    public bool hitWater = false;
+    public GameObject crate;
+
+
+
+
+    void Start()
+    {
+        amin = gameObject.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        // Make GameController and CrateConller wort together
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+
+    }
+
+
+
+
+    // crate hit water
+    void Update()
+    {
+
+
+        if (crate.transform.position.y < -5.4)
+        {
+            crate.SetActive(false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Boat"))
+        {
+            gameController.Metalcrate_Damage();
+            crate.SetActive(false);
+        }
+       
+    }
+}
